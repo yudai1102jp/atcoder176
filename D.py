406837f8d713eca2bc8d593a51be7373fcb0af75
yudai1102jp <yudaiikenoya@gmail.com>
2020-08-25 16:24:07 +0900

    desktop

--- D.py
+++ D.py
@@ -16,13 +16,12 @@ def load():
     for h in range(H):
         for w in range(W):
             if map[h][w] == '.':
-                print(w, h)
                 num = numdec(w, h, max_num)
                 if max_num < num:
                     max_num = num
                 map_num[h][w] = num
 
-    return map_num
+    return map_num, max_num
 
 
 def numdec(w, h, num):
@@ -49,7 +48,7 @@ def numdec(w, h, num):
 
 
 def jadge(w, h):
-    if w < 0 or h < 0 or w > W or h > H:
+    if w < 0 or h < 0 or w >= W or h >= H:
         return 0
     else:
         return map_num[h][w]
@@ -64,5 +63,66 @@ def convert(befornum, afternum):
     return 0
 
 
-map_num = load()
+def search(num):
+    li = set()
+    temp_li = set()
+    for h in range(H):
+        for w in range(W):
+            if map_num[h][w] == num:
+                temp_li = search_sub(w, h, num)
+                li = li | temp_li
+    return {num: li}
+
+
+def search_sub(w, h, num):
+    li_sub = []
+    for i in range(-2, 3):
+        for j in range(-2, 3):
+            w_sub = w + i
+            h_sub = h + j
+            if jadge(h_sub, w_sub) not in [0, num]:
+                li_sub.append(jadge(h_sub, w_sub))
+    return set(li_sub)
+
+
+def search_goal():
+    min_count = 10**6
+    start_num = map_num[start_hw[0]][start_hw[1]]
+    goal_num = map_num[goal_hw[0]][goal_hw[1]]
+    ava = {i+1 for i in range(max_num)}
+    count = search_count(start_num, goal_num, ava)
+    if min_count > count:
+        min_count = count
+    return min_count
+
+
+def search_count(now, go, num_set, count=0):
+    num_set.remove(now)
+    if now == go:
+        return count
+    elif not num_set:
+        return 10**6
+
+    next_set = load_num[now]
+    for num in next_set:
+        global count_list
+        count.append search_(num, go, num_set, count+1)
+    if:
+
+        return count
+
+
+map_num, max_num = load()
+load_num = {}
+for i in range(1, max_num+1):
+    dic = search(i)
+    if dic:
+        load_num.update(dic)
 print(load())
+print(load_num)
+
+count_list = []
+
+search_goal()
+print(ava)
+print(min(count_list))
